19050179a72d1c57f19411c6449bcfb772425d25
Student <example@example.com>
2019-10-29 13:31:17 -0700

    add vowers array and ref to if function

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,22 @@ $(document).ready(function() {
     $('#user-input').hide();
     var userSentence = $('#user-sentence').val();
     var letters = userSentence.split('');
+    var vowels = ["a", "e", "i", "o", "u"]
 
     var puzzle=letters.map(function(letter) {
-      if (letter === "a" || letter === "e"){
+      if (vowels.includes(letter)){
         return('-')
       } else {
         return (letter)
-      }
-    });
-      console.log(puzzle);
+      };
 
+
+
+    });
+    // displaySentence=displaySentence;
+    var displaySentence = puzzle.join(' ');
+    // console.log(displaySentence);
+    $("#results").text(displaySentence);
 
   });
 });
